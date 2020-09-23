57a443592368a36f76088f5e4eb4ce7a1f8c7083
vivek <vivek.koyani@gmail.com>
2020-09-23 23:35:33 +0530

    add schema

--- Back-end/server.js
+++ Back-end/server.js
@@ -10,6 +10,7 @@ const mongoose = require("mongoose");
 var mongoose__Url =
   "mongodb://admin:OHhVZwlzMapIb4bR@cluster0-shard-00-00.kc359.mongodb.net:27017,cluster0-shard-00-01.kc359.mongodb.net:27017,cluster0-shard-00-02.kc359.mongodb.net:27017/curd?ssl=true&replicaSet=atlas-143rmj-shard-0&authSource=admin&retryWrites=true&w=majority";
 
+// connect mongodb  
 mongoose.connect(mongoose__Url, { useNewUrlParser: true }).then(
   () => {
     console.log("Database is connected");
