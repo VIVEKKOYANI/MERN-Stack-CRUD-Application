be9e1e9806b7ab5cbc6a9d7cfef0a579e26a3a44
vivek <vivek.koyani@gmail.com>
2020-09-23 23:27:33 +0530

    connected mongodb

--- Back-end/server.js
+++ Back-end/server.js
@@ -7,8 +7,17 @@ const mongoose = require("mongoose");
 
 //Mongodb Url
 
-var mongoose__Url = "";
+var mongoose__Url =
+  "mongodb://admin:OHhVZwlzMapIb4bR@cluster0-shard-00-00.kc359.mongodb.net:27017,cluster0-shard-00-01.kc359.mongodb.net:27017,cluster0-shard-00-02.kc359.mongodb.net:27017/curd?ssl=true&replicaSet=atlas-143rmj-shard-0&authSource=admin&retryWrites=true&w=majority";
 
+mongoose.connect(mongoose__Url, { useNewUrlParser: true }).then(
+  () => {
+    console.log("Database is connected");
+  },
+  (err) => {
+    console.log("There is a problem" + err);
+  }
+);
 const app = express();
 app.use(bodyparser.json());
 app.use(core());
